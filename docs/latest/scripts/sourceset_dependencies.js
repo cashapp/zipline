f1c98eb6e5b630fe226bbddb273cc3a066f92a6d
JakeWharton <github@jakewharton.com>
2021-08-24 17:18:33 +0000

    Deploying to site from  @ 01184b565c258abcc92feb1a1fb2352cb9959a31 🚀

--- docs/latest/scripts/sourceset_dependencies.js
+++ docs/latest/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies='{":quickjs:jvm:dokkaHtml/main":[]}'
+sourceset_dependencies='{":quickjs:dokkaHtml/androidAndroidTestRelease":[],":quickjs:dokkaHtml/androidDebug":[],":quickjs:dokkaHtml/androidMain":[":quickjs:dokkaHtml/jvmSharedMain",":quickjs:dokkaHtml/commonMain"],":quickjs:dokkaHtml/androidRelease":[],":quickjs:dokkaHtml/commonMain":[],":quickjs:dokkaHtml/jvmMain":[":quickjs:dokkaHtml/jvmSharedMain",":quickjs:dokkaHtml/commonMain"],":quickjs:dokkaHtml/jvmSharedMain":[]}'
