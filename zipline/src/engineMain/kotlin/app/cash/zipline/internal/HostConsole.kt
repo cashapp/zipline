f0d098fdabe170a5396480ff8b1523744c3bc258
Jake Wharton <jw@squareup.com>
2021-10-12 15:20:20 -0400

    Expose HostConsole properly in engineMain
    Currently we rely on the fact that this source set is not truly multiplatform and merely joins JVM or Android to compile.

--- zipline/src/engineMain/kotlin/app/cash/zipline/internal/HostConsole.kt
+++ zipline/src/engineMain/kotlin/app/cash/zipline/internal/HostConsole.kt
@@ -15,17 +15,4 @@
  */
 package app.cash.zipline.internal
 
-import app.cash.zipline.Zipline
-import java.util.logging.Logger
-
-internal actual object HostConsole : Console {
-  private val logger = Logger.getLogger(Zipline::class.qualifiedName)
-
-  override fun log(level: String, message: String) {
-    when (level) {
-      "warn" -> logger.warning(message)
-      "error" -> logger.severe(message)
-      else -> logger.info(message)
-    }
-  }
-}
+internal expect object HostConsole : Console
