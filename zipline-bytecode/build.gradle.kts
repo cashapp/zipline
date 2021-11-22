ddcaeac4656e8e6ff4fc794fecc913197d48b60c
Jesse Wilson <jwilson@squareup.com>
2021-11-22 15:21:54 -0500

    Publish bytecode

--- zipline-bytecode/build.gradle.kts
+++ zipline-bytecode/build.gradle.kts
@@ -1,7 +1,11 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinJvm
+import com.vanniktech.maven.publish.MavenPublishBaseExtension
 
 plugins {
   kotlin("jvm")
   kotlin("plugin.serialization")
+  id("com.vanniktech.maven.publish.base")
 }
 
 dependencies {
@@ -13,4 +17,12 @@ dependencies {
   testImplementation(Dependencies.junit)
   testImplementation(Dependencies.kotlinTest)
   testImplementation(Dependencies.truth)
+}
+
+configure<MavenPublishBaseExtension> {
+  configure(
+    KotlinJvm(
+      javadocJar = JavadocJar.Empty()
+    )
+  )
 }
