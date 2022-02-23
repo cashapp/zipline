2a3088aa94ecc6f9767e4e072d1381a8d3573fa1
Andrew Alexander <adrw@squareup.com>
2022-02-23 11:17:49 -0500

    Cleanup diff

--- zipline-loader/src/nativeMain/kotlin/app/cash/zipline/loader/loaderNative.kt
+++ zipline-loader/src/nativeMain/kotlin/app/cash/zipline/loader/loaderNative.kt
@@ -15,17 +15,7 @@
  */
 package app.cash.zipline.loader
 
-import com.squareup.sqldelight.db.SqlDriver
-import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
+import app.cash.zipline.Zipline
 
-actual class DriverFactory {
-  actual fun createDriver(): SqlDriver {
-    return NativeSqliteDriver(
-      schema = Database.Schema,
-      name = "zipline-loader.db",
-    )
-  }
-}
-
-// TODO find the native exception class
-actual fun isSqlException(e: Exception): Boolean = TODO()
+actual fun Zipline.multiplatformLoadJsModule(bytecode: ByteArray, id: String) =
+  loadJsModule(bytecode, id)
