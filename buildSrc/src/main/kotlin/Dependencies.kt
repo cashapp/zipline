481e2dc75d18fc0a539ca46347b0117f41c83e0e
Veyndan Stuart <veyndan@gmail.com>
2022-04-29 18:24:01 +0200

    Migrate shadowJar to version catalog

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,6 +3,5 @@ object Dependencies {
   val androidMaterial = "com.google.android.material:material:1.4.0"
   val desugarJdkLibs = "com.android.tools:desugar_jdk_libs:1.1.5"
   val mavenPublishGradlePlugin = "com.vanniktech:gradle-maven-publish-plugin:0.18.0"
-  val shadowJarPlugin = "gradle.plugin.com.github.johnrengelman:shadow:7.1.2"
   val sqldelightJdbc = "org.xerial:sqlite-jdbc:3.34.0"
 }
