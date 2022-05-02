6e9c0ccc627d666edd7e9c3328db50b07609b736
Veyndan Stuart <veyndan@gmail.com>
2022-05-02 11:12:42 +0200

    Migrate android-desugarJdkLibs to version catalog

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,5 +1,4 @@
 object Dependencies {
   val androidGradlePlugin = "com.android.tools.build:gradle:7.0.2"
   val androidMaterial = "com.google.android.material:material:1.4.0"
-  val desugarJdkLibs = "com.android.tools:desugar_jdk_libs:1.1.5"
 }
