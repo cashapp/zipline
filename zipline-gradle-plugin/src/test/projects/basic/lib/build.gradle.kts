c23ee8109368e1c37cd4dba8820d328cc2a5b5f3
Jesse Wilson <jwilson@squareup.com>
2022-05-25 16:49:09 -0400

    Upgrade to Kotlin 1.6.21 (#535)
    * Upgrade to Kotlin 1.6.21
    
    * Track module name changes
    
    * Bump the compose version
    
    * Update compile-testing

--- zipline-gradle-plugin/src/test/projects/basic/lib/build.gradle.kts
+++ zipline-gradle-plugin/src/test/projects/basic/lib/build.gradle.kts
@@ -38,16 +38,3 @@ val launchGreetService by tasks.creating(JavaExec::class) {
   classpath = jvmTestRuntimeClasspath
   mainClass.set("app.cash.zipline.tests.LaunchGreetServiceJvmKt")
 }
-
-// TODO: Delete this block once we've upgraded to Kotlin 1.6.20+.
-rootProject.plugins.withType<NodeJsRootPlugin> {
-  val nodeJsRootExtension = rootProject.the<NodeJsRootExtension>()
-
-  // TODO(jwilson): remove this once the Kotlin/JS devserver doesn't crash on boot.
-  // https://stackoverflow.com/questions/69537840/kotlin-js-gradle-plugin-unable-to-load-webpack-cli-serve-command
-  nodeJsRootExtension.versions.webpackCli.version = "4.9.0"
-
-  // TODO(jwilson): remove this once Kotlin's built-in Node.js supports Apple Silicon.
-  //  https://youtrack.jetbrains.com/issue/KT-49109
-  nodeJsRootExtension.nodeVersion = "16.0.0"
-}
