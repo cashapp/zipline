bc2d9bc8e1294e9fba0b8ca5c77befbc4ebd7b72
Andrew Alexander <adrw@squareup.com>
2022-06-13 15:52:12 -0400

    Create new zipline-database subproject

---
+++ zipline-database/src/commonMain/kotlin/app/cash/zipline/database/cacheDatabase.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2022 Block, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package app.cash.zipline.database
+
+import com.squareup.sqldelight.db.SqlDriver
+
+expect class DriverFactory {
+  /** Create a SqlDelight SqlDriver to be used in creating and managing a SqlLite instance on disk */
+  fun createDriver(): SqlDriver
+
+  /** Creates a Database according to generated Schema and runs migrations using a SqlDriver */
+  fun createDatabase(
+    sqlDriver: SqlDriver,
+    schema: SqlDriver.Schema,
+  )
+}
+
+/** Identify if an exception is from platform specific SqlLite library */
+expect fun isSqlException(e: Exception): Boolean
---
+++ zipline-database/src/nativeMain/kotlin/app/cash/zipline/database/cacheDatabaseNative.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2022 Block, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package app.cash.zipline.database
+
+import com.squareup.sqldelight.db.SqlDriver
+import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
+
+actual class DriverFactory {
+  actual fun createDriver(): SqlDriver {
+    return NativeSqliteDriver(
+      schema = Database.Schema,
+      name = "zipline-loader.db",
+    )
+  }
+
+  actual fun createDatabase(
+    sqlDriver: SqlDriver,
+    schema: SqlDriver.Schema
+  ) {
+  }
+}
+
+// TODO find the native exception class
+actual fun isSqlException(e: Exception): Boolean = TODO()
