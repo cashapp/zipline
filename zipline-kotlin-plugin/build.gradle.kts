05285d16f0a99e2a43d3d864def0cd0598047b05
Jesse Wilson <jwilson@squareup.com>
2022-07-26 18:54:52 -0400

    Remove the hosted kotlin compiler plugin

--- zipline-kotlin-plugin/build.gradle.kts
+++ zipline-kotlin-plugin/build.gradle.kts
@@ -1,36 +1,25 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import com.vanniktech.maven.publish.JavadocJar
 import com.vanniktech.maven.publish.KotlinJvm
 import com.vanniktech.maven.publish.MavenPublishBaseExtension
 
 plugins {
   kotlin("jvm")
+  kotlin("kapt")
+  id("com.github.gmazzo.buildconfig")
   id("org.jetbrains.dokka")
   id("com.vanniktech.maven.publish.base")
 }
 
 dependencies {
-  compileOnly(projects.ziplineKotlinPluginHosted)
+  compileOnly(kotlin("compiler-embeddable"))
+
+  kapt("com.google.auto.service:auto-service:1.0")
+  compileOnly("com.google.auto.service:auto-service-annotations:1.0")
 }
 
-val shadowJar = tasks.register("embeddedPlugin", ShadowJar::class.java) {
-  configurations = listOf(project.configurations.getByName("compileClasspath"))
-  relocate("com.intellij", "org.jetbrains.kotlin.com.intellij")
-  archiveBaseName.set("embedded")
-  archiveVersion.set("")
-  destinationDirectory.set(File(buildDir, "repackaged"))
-}
-
-configurations {
-  // replace the standard jar with the one built by 'shadowJar' in both api and runtime variants
-  apiElements.get().outgoing.apply {
-    artifacts.clear()
-    artifact(shadowJar.flatMap { it.archiveFile })
-  }
-  runtimeElements.get().outgoing.apply {
-    artifacts.clear()
-    artifact(shadowJar.flatMap { it.archiveFile })
-  }
+buildConfig {
+  packageName("app.cash.zipline.kotlin")
+  buildConfigField("String", "KOTLIN_PLUGIN_ID", "\"${libs.plugins.zipline.kotlin.get()}\"")
 }
 
 configure<MavenPublishBaseExtension> {
