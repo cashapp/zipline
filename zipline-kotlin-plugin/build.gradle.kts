92ae0f2e79d92ba148ec85e9198a29d691416452
Jake Wharton <jw@squareup.com>
2022-07-27 13:50:50 -0400

    Switch to the embedded Kotlin compiler API
    This removes the need to use the shaodw plugin to relocate our bytecode to match.

--- zipline-kotlin-plugin/build.gradle.kts
+++ zipline-kotlin-plugin/build.gradle.kts
@@ -1,4 +1,3 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import com.vanniktech.maven.publish.JavadocJar
 import com.vanniktech.maven.publish.KotlinJvm
 import com.vanniktech.maven.publish.MavenPublishBaseExtension
@@ -9,11 +8,11 @@ plugins {
   id("com.github.gmazzo.buildconfig")
   id("org.jetbrains.dokka")
   id("com.vanniktech.maven.publish.base")
-  id("com.github.johnrengelman.shadow")
 }
 
 dependencies {
-  compileOnly(kotlin("compiler"))
+  compileOnly(kotlin("compiler-embeddable"))
+  compileOnly(kotlin("stdlib"))
 
   kapt("com.google.auto.service:auto-service:1.0")
   compileOnly("com.google.auto.service:auto-service-annotations:1.0")
@@ -22,23 +21,6 @@ dependencies {
 buildConfig {
   packageName("app.cash.zipline.kotlin")
   buildConfigField("String", "KOTLIN_PLUGIN_ID", "\"${libs.plugins.zipline.kotlin.get()}\"")
-}
-
-val shadowJar = tasks.named("shadowJar", ShadowJar::class.java)
-shadowJar.configure {
-  relocate("com.intellij", "org.jetbrains.kotlin.com.intellij")
-}
-
-configurations {
-  // replace the standard jar with the one built by 'shadowJar' in both api and runtime variants
-  apiElements.get().outgoing.apply {
-    artifacts.clear()
-    artifact(shadowJar.flatMap { it.archiveFile })
-  }
-  runtimeElements.get().outgoing.apply {
-    artifacts.clear()
-    artifact(shadowJar.flatMap { it.archiveFile })
-  }
 }
 
 configure<MavenPublishBaseExtension> {
