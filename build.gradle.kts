61a2c37609cc69ad304702a5e63c11b2d3eb24ed
Jesse Wilson <jwilson@squareup.com>
2022-12-06 12:47:49 -0500

    Prepare next development version

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ apply(plugin = "com.vanniktech.maven.publish.base")
 
 allprojects {
   group = "app.cash.zipline"
-  version = "0.9.12"
+  version = "1.0.0-SNAPSHOT"
 
   repositories {
     mavenCentral()
