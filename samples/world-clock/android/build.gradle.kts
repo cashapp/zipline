2e7e3af6b5f0a91e5d19219f2d72188942be5de3
shabinder <shabinder.singh@theatom.app>
2023-08-15 17:38:59 +0530

    Use min-sdk from toml for world-clock.

--- samples/world-clock/android/build.gradle.kts
+++ samples/world-clock/android/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
   defaultConfig {
     applicationId = "com.example.zipline.worldclock"
-    minSdk = libs.versions.compileSdk.get().toInt()
+    minSdk = libs.versions.minSdk.get().toInt()
   }
 
   compileOptions {
