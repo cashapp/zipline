4ee2ce867ee36b6fb0e3bb4ad066511f40058e12
Jesse Wilson <jwilson@squareup.com>
2023-12-19 13:34:12 -0500

    WIP: play with SecureRandom and multiplatform

---
+++ zipline/src/hostTest/kotlin/app/cash/zipline/SecureRandomTest.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2023 Cash App
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package app.cash.zipline
+
+import kotlin.test.Test
+
+class SecureRandomTest {
+  @Test
+  fun happyPath() {
+    val sink = ByteArray(10)
+    secureRandom().nextBytes(sink)
+    println(sink.contentToString())
+  }
+}
+
+interface SecureRandom {
+  fun nextBytes(sink: ByteArray)
+}
+
+expect fun secureRandom(): SecureRandom
+
