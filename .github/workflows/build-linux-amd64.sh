49ffa212e6f6fd7126a8f71e470559ab47ab6a36
Jake Wharton <jw@squareup.com>
2024-07-08 13:14:25 -0400

    Build directly on Ubuntu 20.04

--- .github/workflows/build-linux-amd64.sh
+++ .github/workflows/build-linux-amd64.sh
@@ -11,22 +11,12 @@
 # work for our users. We also need a compiler that's new-enough that it has
 # `stdatomic.h`. Yuck.
 
-set -e
-set -x
-
-cd /zipline
-yum -y install centos-release-scl
-yum -y install devtoolset-7 devtoolset-7-toolchain
-. /opt/rh/devtoolset-7/enable
-yum -y install epel-release
-yum -y install cmake3
-yum -y install java-1.8.0-openjdk-devel
+set -ex
 
 mkdir -p build/jni/amd64/
 mkdir -p zipline/src/jvmMain/resources/jni/amd64/
-cmake3 -S zipline/src/jvmMain/ -B build/jni/amd64/ \
+cmake -S zipline/src/jvmMain/ -B build/jni/amd64/ \
   -DQUICKJS_VERSION="$(cat zipline/native/quickjs/VERSION)" \
-  -DCMAKE_SYSTEM_PROCESSOR=x86_64 \
-  -DCMAKE_C_COMPILER=/opt/rh/devtoolset-7/root/usr/bin/gcc \
-  -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-7/root/usr/bin/c++
-cmake3 --build build/jni/amd64/ --verbose
+  -DCMAKE_SYSTEM_PROCESSOR=x86_64
+cmake --build build/jni/amd64/ --verbose
+cp -v build/jni/amd64/libquickjs.* zipline/src/jvmMain/resources/jni/amd64/
