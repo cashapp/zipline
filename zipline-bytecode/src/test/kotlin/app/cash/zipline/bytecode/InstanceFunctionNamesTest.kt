869dc4d48454372f942af247bc0af13d7c7b71d6
Jesse Wilson <jwilson@squareup.com>
2025-07-09 15:06:01 -0400

    Test for function names on instance functions

---
+++ zipline-bytecode/src/test/kotlin/app/cash/zipline/bytecode/InstanceFunctionNamesTest.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2023 Cash App
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package app.cash.zipline.bytecode
+
+import app.cash.zipline.QuickJs
+import assertk.assertThat
+import assertk.assertions.startsWith
+import kotlin.test.assertFailsWith
+import org.junit.After
+import org.junit.Test
+
+class InstanceFunctionNamesTest {
+  private val quickJs = QuickJs.create()
+
+  @After
+  fun tearDown() {
+    quickJs.close()
+  }
+
+  @Test
+  fun happyPath() {
+    val js = """
+      |function Bomb(message) {
+      |  this.message = message;
+      |}
+      |
+      |Bomb.prototype.sayHello = function() {
+      |  this.goBoom3();
+      |}
+      |
+      |Bomb.prototype.goBoom3 = function() {
+      |  this.goBoom2();
+      |}
+      |
+      |Bomb.prototype.goBoom2 = function() {
+      |  this.goBoom1();
+      |}
+      |
+      |Bomb.prototype.goBoom1 = function() {
+      |  throw Error(this.message);
+      |}
+      """.trimMargin()
+
+    val bytecode = quickJs.compile(js, "demo.js")
+    val updatedBytecode = stripLineNumbers(bytecode)
+    quickJs.execute(updatedBytecode)
+    val exception = assertFailsWith<Exception> {
+      quickJs.evaluate("new Bomb(\"boom!\").sayHello()")
+    }
+    assertThat(exception.stackTraceToString()).startsWith(
+      """
+      |app.cash.zipline.QuickJsException: boom!
+      |	at JavaScript.goBoom1(demo.js)
+      |	at JavaScript.goBoom2(demo.js)
+      |	at JavaScript.goBoom3(demo.js)
+      |	at JavaScript.sayHello(demo.js)
+      |	at JavaScript.<eval>(?)
+      """.trimMargin(),
+    )
+  }
+}
