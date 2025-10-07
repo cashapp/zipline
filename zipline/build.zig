576074eba2c1b89f748b66e5a8d18fadede86297
Jake Wharton <jw@squareup.com>
2025-10-06 21:06:22 -0400

    Windows ARM

--- zipline/build.zig
+++ zipline/build.zig
@@ -9,6 +9,7 @@ pub fn build(b: *std.Build) !void {
   try setupTarget(b, &deleteLib.step, .linux, .x86_64, "amd64");
   try setupTarget(b, &deleteLib.step, .macos, .aarch64, "aarch64");
   try setupTarget(b, &deleteLib.step, .macos, .x86_64, "x86_64");
+  try setupTarget(b, &deleteLib.step, .windows, .aarch64, "aarch64");
   try setupTarget(b, &deleteLib.step, .windows, .x86_64, "amd64");
 }
 
